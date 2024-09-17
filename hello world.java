ff4ba73eecbc73b0814c4fada1171b09c7a14b4a
Mell0_64 <117129227+Mello46514@users.noreply.github.com>
2024-09-17 11:46:52 -0700

    its the JAVA!!!

---
+++ hello world.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        Scanner myObj1 = new Scanner(System.in);
+        System.out.println("Hello World!\ni hate java.\nhere's some python code!\n\npython console\n>>>>>>>print(\"Hello world!\")\nHello world!\n\n see? its easy, why do you need to go\npublic class main{\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Hello world!\");\n\t}\n}\n\nITS SO STUPID!!!");
+        System.out.println("You can talk now btw,");
+        String response = myObj1.nextLine();
+        System.out.println(response + " is the right awnser!");
+        System.out.println("so what u doin?");
+        Scanner myObj2 = new Scanner(System.in);
+        response = myObj2.nextLine();
+        System.out.println("I LOVE " + response);
+        System.out.println("anyways, goodbye!");
+        Scanner myObj3 = new Scanner(System.in);
+        response = myObj3.nextLine();
+        System.out.println("and so the fake (java based) chatbot went, to go ramble about how saying hello world in java is so bad, soon he would realise that it isnt too bad, as its just one step harder than C, and C++ is a nightmare. it found peace in itself.\nthanks for playing!");
+    }
+}
